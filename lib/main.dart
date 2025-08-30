36adb55de44992ecf5e42566ee5164457335997e
Christine Joyce Belen <christinejoycebelen@gmail.com>
2025-08-30 18:18:45 +0800

    Initial Flutter code

---
+++ lib/main.dart
@@ -0,0 +1,24 @@
+import 'package:flutter/material.dart';
+
+void main () {
+  runApp(App());
+}
+
+class App extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+Scaffold homepage = Scaffold(
+  appBar: AppBar(title: Text('Homepage'))
+);
+
+    return MaterialApp(
+      home: homepage,
+      theme: ThemeData(
+        appBarTheme: AppBarTheme(
+          backgroundColor: Colors.red,
+          foregroundColor: Colors.white
+        )
+      ),
+    );
+  }
+}
